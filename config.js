8791d59ae123e0140511fd242219b1f66de01254
Ishara2010 <nameishara@gmail.com>
2024-10-26 05:17:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=q2xAELYQ#0Gw1B8Nweo28zGymMfiQh0YYdj9gx5f2NSb0kOdPmbc",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=qugByRRQ#i-LGDaARgkBYhbD6sBwN3yw5UhHrrx7ashcoQLwVGqA",
 MONGODB: process.env.MONGODB || "mongodb+srv://nameishara:gKEy6jKkFKg8Ribf@cluster0.9mkcf.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
